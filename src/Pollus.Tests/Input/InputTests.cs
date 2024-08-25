4e9477969e00286f7a1b936b6c4793c2bb7a89dc
Ole-Gustav Røed <refsa.dev@gmail.com>
2024-08-25 17:20:34 +0200

    test: fix InputTests

--- src/Pollus.Tests/Input/InputTests.cs
+++ src/Pollus.Tests/Input/InputTests.cs
@@ -1,5 +1,6 @@
 namespace Pollus.Tests.Input;
 
+using Pollus.ECS;
 using Pollus.Engine.Input;
 
 public class InputTests
@@ -7,34 +8,42 @@ public class InputTests
     [Fact]
     public void Test_Keyboard()
     {
+        var events = new Events();
+        events.InitEvent<ButtonEvent<MouseButton>>();
+        events.InitEvent<AxisEvent<MouseAxis>>();
+        events.InitEvent<ButtonEvent<GamepadButton>>();
+        events.InitEvent<AxisEvent<GamepadAxis>>();
+        events.InitEvent<ButtonEvent<Key>>();
+        events.InitEvent<MouseMovedEvent>();
+
         var keyboard = new Keyboard();
 
         keyboard.SetKeyState(Key.KeyA, true);
-        keyboard.Update();
+        keyboard.Update(events);
         Assert.True(keyboard.JustPressed(Key.KeyA));
         keyboard.SetKeyState(Key.KeyA, false);
-        keyboard.Update();
+        keyboard.Update(events);
         Assert.True(keyboard.JustReleased(Key.KeyA));
 
         keyboard.SetKeyState(Key.KeyA, true);
-        keyboard.Update();
+        keyboard.Update(events);
         keyboard.SetKeyState(Key.KeyA, true);
-        keyboard.Update();
+        keyboard.Update(events);
         Assert.True(keyboard.Pressed(Key.KeyA));
         keyboard.SetKeyState(Key.KeyA, false);
-        keyboard.Update();
+        keyboard.Update(events);
         Assert.True(keyboard.JustReleased(Key.KeyA));
 
         keyboard.SetKeyState(Key.KeyA, true);
-        keyboard.Update();
+        keyboard.Update(events);
         for (int i = 0; i < 100; i++)
         {
             keyboard.SetKeyState(Key.KeyA, true);
-            keyboard.Update();
+            keyboard.Update(events);
             Assert.True(keyboard.Pressed(Key.KeyA));
         }
         keyboard.SetKeyState(Key.KeyA, false);
-        keyboard.Update();
+        keyboard.Update(events);
         Assert.True(keyboard.JustReleased(Key.KeyA));
     }
 }
