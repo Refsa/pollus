95b19692fcef7f071edf1eef0e3903f8bd9ae965
Ole-Gustav Røed <refsa.dev@gmail.com>
2024-09-05 23:02:07 +0200

    Squashed commit of the following:
    commit e76a659ff0db2b5728a110817d4563a49b340244
    Author: Ole-Gustav Røed <refsa.dev@gmail.com>
    Date:   Thu Sep 5 22:55:57 2024 +0200
    
        ecs: ISystemParam to wrap multiple system params in a single one

--- src/Pollus.ECS/SystemParam.cs
+++ src/Pollus.ECS/SystemParam.cs
@@ -37,38 +37,3 @@ public struct Param<T0> : IFetch<Param<T0>>, ISystemParam
         return new Param<T0> { Param0 = t0 };
     }
 }
-
-public struct Param<T0, T1> : IFetch<Param<T0, T1>>, ISystemParam
-{
-    static readonly HashSet<Type> dependencies;
-    public static HashSet<Type> Dependencies => dependencies;
-    static readonly Fetch.Info t0Fetch;
-    static readonly Fetch.Info t1Fetch;
-    static Param()
-    {
-#pragma warning disable IL2059
-        RuntimeHelpers.RunClassConstructor(typeof(T0).TypeHandle);
-        RuntimeHelpers.RunClassConstructor(typeof(T1).TypeHandle);
-#pragma warning restore IL2059
-        t0Fetch = Fetch.Get<T0>();
-        t1Fetch = Fetch.Get<T1>();
-        dependencies = [.. t0Fetch.Dependencies, .. t1Fetch.Dependencies];
-
-        Register();
-    }
-
-    public static void Register()
-    {
-        Fetch.Register<Param<T0, T1>>(new Param<T0, T1>(), [.. dependencies]);
-    }
-
-    public T0 Param0;
-    public T1 Param1;
-
-    public Param<T0, T1> DoFetch(World world, ISystem system)
-    {
-        var t0 = ((IFetch<T0>)t0Fetch.Fetch).DoFetch(world, system);
-        var t1 = ((IFetch<T1>)t1Fetch.Fetch).DoFetch(world, system);
-        return new Param<T0, T1> { Param0 = t0, Param1 = t1 };
-    }
-}
