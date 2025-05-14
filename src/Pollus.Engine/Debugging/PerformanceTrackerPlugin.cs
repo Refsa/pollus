c4c54dcba07352516e19896c420f0a4aed8c9fb9
Ole-Gustav Røed <refsa.dev@gmail.com>
2025-05-14 20:59:28 +0200

    core: track avg frametime

--- src/Pollus.Engine/Debugging/PerformanceTrackerPlugin.cs
+++ src/Pollus.Engine/Debugging/PerformanceTrackerPlugin.cs
@@ -2,19 +2,39 @@ namespace Pollus.Engine.Debug;
 
 using Pollus.Debugging;
 using Pollus.ECS;
+using Pollus.Graphics.Windowing;
 
 public class PerformanceTrackerPlugin : IPlugin
 {
+    public class PerformanceMetrics
+    {
+        readonly Queue<float> frameTimes = new();
+        float frameTime;
+
+        public float FrameTime => frameTime;
+        public float AverageFrameTime => frameTimes.Average();
+        public float AverageFPS => 1f / AverageFrameTime;
+
+        public void AddFrameTime(float frameTime)
+        {
+            this.frameTime = frameTime;
+            frameTimes.Enqueue(frameTime);
+            if (frameTimes.Count > int.Max(60, (int)(1f / frameTime * 4))) frameTimes.Dequeue();
+        }
+    }
+
     public void Apply(World world)
     {
+        world.Resources.Add(new PerformanceMetrics());
+
         world.Schedule.AddSystems(CoreStage.Last, FnSystem.Create(new("LogPerformanceMetrics")
         {
             RunCriteria = new RunFixed(1f)
         },
-        static (Time time) =>
+        static (IWindow window, PerformanceMetrics metrics, Time time) =>
         {
-            var fps = 1f / time.DeltaTime;
-            Log.Info($"FPS: {fps}");
+            metrics.AddFrameTime(time.DeltaTimeF);
+            window.SetTitle($"Pollus - {metrics.AverageFPS:F2} FPS");
         }));
     }
 }
